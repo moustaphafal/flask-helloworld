52bf16b209b5b9146c17dbf293843f9026c4bf81
moustaphafal <tontonfall5@gmail.com>
2025-10-20 22:45:00 +0000

    Initial commit for simple Flask user management app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+users = []
+
+@app.route('/')
+def hello():
+    return render_template('index.html', users=users)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form.get('name')
+    if name:
+        users.append(name)
+    return redirect(url_for('hello'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
